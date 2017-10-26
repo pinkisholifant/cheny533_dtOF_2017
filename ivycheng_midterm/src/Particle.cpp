6ae5f467a095833f135eea052a0bd75ef960bf92
pinkisholifant <30579057+pinkisholifant@users.noreply.github.com>
2017-10-26 18:28:57 -0400

    midterm
    emotion flow on union square

---
+++ ivycheng_midterm/src/Particle.cpp
@@ -0,0 +1,63 @@
+
+#include "Particle.hpp"
+
+Particle::Particle()
+{
+}
+
+Particle::Particle(ofVec3f _pos, ofVec3f _vel)
+{
+	pos = _pos;
+	vel = _vel;
+}
+
+void Particle::applyForce(ofVec3f force)
+{
+	acc += force*0.6;
+}
+
+void Particle::applyDamping(float strength)
+{
+	acc -= vel.getNormalized() * strength;
+}
+
+void Particle::update()
+{
+	vel += acc;
+	pos += vel;
+    if(pos.x < 0 || pos.x > ofGetWidth()){
+        vel.x *=-1;
+        vel += -acc;
+    }
+    if(pos.y < 0 || pos.y > ofGetHeight()){
+        vel.y *= -1;
+        vel += -acc;
+            }
+
+	acc *= 0;
+}
+void star(float x, float y, float radius1, float radius2, int npoints) {
+    float angle = TWO_PI / npoints;
+    float halfAngle = angle/2.0;
+    ofBeginShape();
+    for (float a = 0; a < TWO_PI; a += angle) {
+        float sx = x + cos(a) * radius2;
+        float sy = y + sin(a) * radius2;
+        ofCurveVertex(sx, sy);
+        sx = x + cos(a+halfAngle) * radius1;
+        sy = y + sin(a+halfAngle) * radius1;
+        ofCurveVertex(sx, sy);
+    }
+    ofEndShape();
+}
+
+void Particle::draw(){
+	
+	ofPushStyle();
+	ofSetColor(ofRandom(0,255),0,0);
+    star(pos.x,pos.y, 4, 8, 7);
+
+	//ofDrawCircle(pos, 1);
+	ofPopStyle();
+}
+
